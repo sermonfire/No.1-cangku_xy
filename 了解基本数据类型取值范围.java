2d2350bb51e5b9ddec4d6181bd133f466f78d642
sermonfire <2900450395@qq.com>
2023-05-22 16:54:32 +0800

    了解基本数据类型取值范围.java

---
+++ 了解基本数据类型取值范围.java
@@ -0,0 +1,14 @@
+package chapter3;
+
+public class 了解基本数据类型取值范围 {
+
+	public static void main(String[] args) {
+		System.out.println("byte:"+Byte.MIN_VALUE+"~"+Byte.MAX_VALUE);
+		System.out.println("short:"+Short.MIN_VALUE+"~"+Short.MAX_VALUE);
+		System.out.println("int:"+Integer.MIN_VALUE+"~"+Integer.MAX_VALUE);
+		System.out.println("long:"+Long.MIN_VALUE+"~"+Long.MAX_VALUE);
+		System.out.println("float:"+Float.MIN_VALUE+"~"+Float.MAX_VALUE);
+		System.out.println("double:"+Double.MIN_VALUE+"~"+Double.MAX_VALUE);
+	}
+
+}
